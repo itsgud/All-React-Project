6b5cb1da93b990c1752dbeccc43d993145663e5b
itsgud <92721403+itsgud@users.noreply.github.com>
2023-10-27 21:22:35 +0530

    Add files via upload

---
+++ gs3/src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react" ;
+
+class App extends Component{
+    render(){
+        return  React.createElement
+        ("h1",null,"hello worldd !!!");
+    }
+}
+
+export default App;
